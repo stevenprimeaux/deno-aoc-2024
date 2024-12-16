a6ab270b7e3510e8d97aed3384a37dc61e0d3e67
stevenprimeaux <stevenprimeaux@gmail.com>
2024-12-15 20:17:05 -0500

    Relax condition so that array with one bad element is still safe.

--- countIsSafe.ts
+++ countIsSafe.ts
@@ -8,8 +8,66 @@ export function isSafe(array: number[]): boolean {
   }
 }
 
+export function isSafe1(array: number[]): boolean {
+  let isSafe: boolean;
+  let arrayTry0 = array;
+  let arrayTry1 = array;
+
+  // assume decreasing safe
+  isSafe = true;
+  for (let i = 1; i < array.length; i++) {
+    if (!isGoodDec(array[i] - array[i - 1])) {
+      isSafe = false;
+      arrayTry0 = array.toSpliced(i - 1, 1);
+      arrayTry1 = array.toSpliced(i, 1);
+      break;
+    }
+  }
+  if (isSafe) {
+    return true;
+  } else {
+    isSafe = arrayTry0.map((e, i, a) =>
+      e - a[i - 1]
+    ).slice(1).every(isGoodDec) || arrayTry1.map((e, i, a) =>
+      e - a[i - 1]
+    ).slice(1).every(isGoodDec);
+    if (isSafe) {
+      return true;
+    }
+  }
+
+  // assume increasing safe
+  isSafe = true;
+  for (let i = 1; i < array.length; i++) {
+    if (!isGoodInc(array[i] - array[i - 1])) {
+      isSafe = false;
+      arrayTry0 = array.toSpliced(i - 1, 1);
+      arrayTry1 = array.toSpliced(i, 1);
+      break;
+    }
+  }
+  if (isSafe) {
+    return true;
+  } else {
+    isSafe = arrayTry0.map((e, i, a) =>
+      e - a[i - 1]
+    ).slice(1).every(isGoodInc) || arrayTry1.map((e, i, a) =>
+      e - a[i - 1]
+    ).slice(1).every(isGoodInc);
+    if (isSafe) {
+      return true;
+    }
+  }
+
+  return isSafe;
+}
+
 export function countIsSafe(array: number[][]): number {
   return array.filter(isSafe).length;
+}
+
+export function countIsSafe1(array: number[][]): number {
+  return array.filter(isSafe1).length;
 }
 
 function isGoodDec(diff: number): boolean {
--- countIsSafe_test.ts
+++ countIsSafe_test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "@std/assert";
 
 import { arraysFromFileRows } from "./util.ts";
 
-import { countIsSafe, isSafe } from "./countIsSafe.ts";
+import { countIsSafe, countIsSafe1, isSafe, isSafe1 } from "./countIsSafe.ts";
 
 Deno.test(function isSafeTest() {
   assertEquals(isSafe([1, 4, 7]), true);
@@ -14,7 +14,22 @@ Deno.test(function isSafeTest() {
   assertEquals(isSafe([2, 1, 2]), false);
 });
 
+Deno.test(function isSafe1Test() {
+  assertEquals(isSafe1([1, 4, 7]), true);
+  assertEquals(isSafe1([7, 4, 1]), true);
+  assertEquals(isSafe1([1, 1, 1]), false);
+  assertEquals(isSafe1([1, 4, 8]), true);
+  assertEquals(isSafe1([8, 4, 1]), true);
+  assertEquals(isSafe1([1, 2, 1]), true);
+  assertEquals(isSafe1([2, 1, 2]), true);
+});
+
 Deno.test(async function countIsSafeTest() {
   const array = await arraysFromFileRows("aoc_02.txt");
   assertEquals(countIsSafe(array), 246);
+});
+
+Deno.test(async function countIsSafe1Test() {
+  const array = await arraysFromFileRows("aoc_02.txt");
+  assertEquals(countIsSafe1(array), 318);
 });
